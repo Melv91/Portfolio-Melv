be6f74bfe805415af6d416959bef79dfc278fa57
Melv91 <nivlemtcng@gmail.com>
2025-01-11 02:04:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function scrollToSection() {
+    // Trouve la section
+    const section = document.getElementById('section2');
+    
+    // Défile jusqu'à la section avec un effet fluide
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: 'smooth'
+    });
+  }
+
+
+  function scrollToSection2() {
+    // Trouve la section
+    const section = document.getElementById('section3');
+    
+    // Défile jusqu'à la section avec un effet fluide
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: 'smooth'
+    });
+  }
+
+
+  function scrollToSection3() {
+    // Trouve la section
+    const section = document.getElementById('sectionHome');
+    
+    // Défile jusqu'à la section avec un effet fluide
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: 'smooth'
+    });
+  }
+
+  function scrollToSection4() {
+    // Trouve la section
+    const section = document.getElementById('section5');
+    
+    // Défile jusqu'à la section avec un effet fluide
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: 'smooth'
+    });
+  }
+
+  function scrollToSection5() {
+    // Trouve la section
+    const section = document.getElementById('section4');
+    
+    // Défile jusqu'à la section avec un effet fluide
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: 'smooth'
+    });
+  }
+
+
+  
